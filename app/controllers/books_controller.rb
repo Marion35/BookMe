be50c5553eab00f30615f7692c626476591e05fb
marion thorin <marion.thorin@gmail.com>
2023-12-19 19:05:13 +0100

    finish back

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,13 +13,31 @@ class BooksController < ApplicationController
   end
 
   def create
-    @book = Book.new(params[:id])
+    @book = Book.new(book_params)
+    @book.user = current_user
     @book.save
     redirect_to books_path(@book)
   end
 
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book)
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to book_path(@book), status: :see_other
+  end
+
 
   private
+
 
   def book_params
     params.require(:book).permit(:title, :author, :description, :user_id)
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,16 +8,26 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
+puts "destroy Books"
+Book.destroy_all
+User.destroy_all
+
 puts "user"
 
-user1 = User.create!(email: "marionthorin@email.com", password: "123456")
-user2 = User.create!(email: "guillaumegans@email.com", password: "123456")
+user3 = User.create!(email: "marionthorin@email.com", password: "123456")
+user4 = User.create!(email: "guillaumegans@email.com", password: "123456")
+user5 = User.create!(email: "marionthorin@gmail.com", password: "123456")
+user6 = User.create!(email: "guillaumegans@gmail.com", password: "123456")
+user7 = User.create!(email: "marion@email.com", password: "123456")
+user8 = User.create!(email: "guillaume@email.com", password: "123456")
+
+
 
 
 puts "books"
 
-book1 = Book.create!(title: "La fille du train", author: "Paula Hawkins", description: "Roman d'enquête", user_id: user1.id)
-book2 = Book.create!(title: "Astérix le Gaulois", author: "R. Goscinny", description: "BD française très connue, de style comédie", user_id: user1.id)
-book3 = Book.create!(title: "Le recueil de Poèmes", author: "Laetitia Girod", description: "Livre de poèmes", user_id: user1.id)
-book4 = Book.create!(title: "Le choeur des femmes", author: "Martin Winckler", description: "Roman parlant de médecine, on suit une jeune fille en stage", user_id: user1.id)
-book5 = Book.create!(title: "Vertiges", author: "Robin Cook", description: "Roman de médecine, des personnes ont des vertiges mais on ne sait pas pourquoi", user_id: user1.id)
+book1 = Book.create!(title: "La fille du train", author: "Paula Hawkins", description: "Roman d'enquête", user_id: user3.id)
+book2 = Book.create!(title: "Astérix le Gaulois", author: "R. Goscinny", description: "BD française très connue, de style comédie", user_id: user3.id)
+book3 = Book.create!(title: "Le recueil de Poèmes", author: "Laetitia Girod", description: "Livre de poèmes", user_id: user3.id)
+book4 = Book.create!(title: "Le choeur des femmes", author: "Martin Winckler", description: "Roman parlant de médecine, on suit une jeune fille en stage", user_id: user3.id)
+book5 = Book.create!(title: "Vertiges", author: "Robin Cook", description: "Roman de médecine, des personnes ont des vertiges mais on ne sait pas pourquoi", user_id: user3.id)
